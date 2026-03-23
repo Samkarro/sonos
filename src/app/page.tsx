ebd5e2eb8fc85ea84a136de9dff1a6ebc972d1c4
Luka Gogichaishvili <luka595664@gmail.com>
2026-03-23 21:03:35 +0400

    feat: init pixi app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { useEffect, useRef } from "react";
+import "./editor.styles.css";
+import * as PIXI from "pixi.js";
+
 export default function Home() {
-  return <main>Hello</main>;
+  const canvasRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const app = new PIXI.Application({
+      width: 1920,
+      height: 1080,
+      autoDensity: true,
+      resolution: 1,
+    });
+
+    canvasRef.current.appendChild(app.view as HTMLCanvasElement);
+
+    return () => {
+      app.destroy(true);
+    };
+  }, []);
+
+  return (
+    <div className="view">
+      <div className="controls-section-container">Hey</div>
+      <div className="canvas-section-container">
+        <div className="canvas-container" ref={canvasRef}></div>
+      </div>
+    </div>
+  );
 }
